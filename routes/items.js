a205c95510c7ec8e60d18b330fe521a09acbb5c8
Matvey Korotygin <matveykaklanter@mail.ru>
2023-07-16 12:50:35 +0200

    Add search, create logic

--- routes/items.js
+++ routes/items.js
@@ -1,16 +1,45 @@
 const express = require('express')
 const router = express.Router()
+const Item = require('../models/item')
 
-router.get('/', (req, res) => {
-    res.render('items/index')
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name != '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const items = await Item.find(searchOptions)
+        res.render('items/index', {
+            items: items,
+            searchOptions: req.query
+        })
+    } catch (error) {
+        res.redirect('/')   
+    }
+    
 })
 
 router.get('/new', (req, res) => { 
-    res.render('items/new')
+    res.render('items/new', { item: new Item() })
 })
 
-router.post('/', (req, res) => { 
-    res.send('Create')
+router.post('/', async (req, res) => {
+    const item = new Item({
+        name: req.body.name,
+        siteUrl: req.body.siteUrl,
+        githubUrl: req.body.githubUrl,
+        youtubeUrl: req.body.youtubeUrl,
+        tags: req.body.tags
+    })
+    try {
+        const newItem = await item.save()
+        // res.redirect(`items/${newItem.id}`)
+    } catch (error) {
+        res.render('items/new', {
+            item: item,
+            errorMessage: 'Error creating Item'
+        })
+    }
 })
 
 module.exports = router
